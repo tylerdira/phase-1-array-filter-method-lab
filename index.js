a9bc6679c1c937818fcd5fe4731273384e4370ec
Joseph Tyler DiRaffaele <tylerdiraffaele@gmail.com>
2022-06-30 10:54:28 -0700

    commit tree fc55fa9f4da19e97066e54e4a933a26e3984a959

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Code your solution here
+function findMatching(arrayOfDrivers, string){
+    let listOfDrivers = arrayOfDrivers.filter(names => names.toLowerCase() === string.toLowerCase())
+    if(listOfDrivers) {
+        return listOfDrivers
+    } else {
+        return undefined
+    }
+    
+}
+
+
+function fuzzyMatch(arrayOfDrivers, string){
+    let listOfDrivers = arrayOfDrivers.filter(names => names[0] === string[0])
+    if(listOfDrivers) {
+        return listOfDrivers
+    } else {
+        return undefined
+    }
+    
+}
+
+function matchName(arrayOfDrivers, string){
+    let listOfDrivers = arrayOfDrivers.filter(names => names.name === string)
+    if(listOfDrivers) {
+        return listOfDrivers
+    } else {
+        return undefined
+    }
+}
